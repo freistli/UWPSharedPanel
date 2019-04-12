33c99bd5518a152f7fa837330a11839adb81d54f
Freist Li <freistli@microsoft.com>
2019-04-12 16:14:54 +0800

    started

---
+++ TestWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Toolkit.Wpf.UI.XamlHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWPFApp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SharedXamlHost_ChildChanged(object sender, EventArgs e)
+        {
+            WindowsXamlHost windowsXamlHost = (WindowsXamlHost)sender;
+
+            UWPSharedPanel.RealEstateComponent sharedPanel =
+                (UWPSharedPanel.RealEstateComponent)windowsXamlHost.Child;        
+             
+        }
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            SharedXamlHost.Dispose();
+        }
+    }
+}
